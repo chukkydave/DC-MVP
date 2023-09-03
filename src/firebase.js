c8e7f5af28c940a61ee6632303a6d2114beefcfa
chukkydave <chukkydave@gmail.com>
2023-09-03 16:13:51 +0100

    initial comit including initialization of firebase and auth login and logout

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// import firebase from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCEFQc77Gxa7yzfPdTEzd3BNeH8Jhhi-OI",
+    authDomain: "dc-mvp.firebaseapp.com",
+    projectId: "dc-mvp",
+    storageBucket: "dc-mvp.appspot.com",
+    messagingSenderId: "316129031315",
+    appId: "1:316129031315:web:6e9cdb7c897ac746bec966"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const db = firebase.firestore();
+// const auth = firebase.auth();
+// // Export Firebase services for use throughout your app
+// export { firebase, db, auth };
+export const auth = getAuth(app);
+export default app;
